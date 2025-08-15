7fdd950701b3c6cdfb9e9c3e8efad2a7f0362fec
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-08-15 09:40:51 -0400

    Commit missingness by component plots 📊

--- figures/missing_by_component.R
+++ figures/missing_by_component.R
@@ -13,20 +13,37 @@ all_data = read.csv("~/Documents/Allostatic_load_audits/ICD-Codes/all_ali_dat_w_
                                        true = ALI_COMPONENT, 
                                        false = CHART_ALI_COMPONENT)) |> 
   select(PAT_MRN_ID, Variable_Name, ALI_COMPONENT, SUPP_ALI_COMPONENT, CHART_ALI_COMPONENT) |> 
-  pivot_longer(cols = c(ALI_COMPONENT, SUPP_ALI_COMPONENT, CHART_ALI_COMPONENT), 
+  rename(ORIG_ALI_COMPONENT = SUPP_ALI_COMPONENT) |> 
+  left_join(
+    read.csv("~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_llm_nocontext_validated.csv") |> 
+      mutate(CHART_ALI_COMPONENT = if_else(condition = !is.na(ALI_COMPONENT) & is.na(CHART_ALI_COMPONENT), 
+                                           true = ALI_COMPONENT, 
+                                           false = CHART_ALI_COMPONENT)) |> 
+      select(PAT_MRN_ID, Variable_Name, ALI_COMPONENT, SUPP_ALI_COMPONENT, CHART_ALI_COMPONENT) |> 
+      rename(LLM_ALI_COMPONENT = SUPP_ALI_COMPONENT)
+  ) |> 
+  left_join(
+    read.csv("~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_llm_context_validated.csv") |> 
+      mutate(CHART_ALI_COMPONENT = if_else(condition = !is.na(ALI_COMPONENT) & is.na(CHART_ALI_COMPONENT), 
+                                           true = ALI_COMPONENT, 
+                                           false = CHART_ALI_COMPONENT)) |> 
+      select(PAT_MRN_ID, Variable_Name, ALI_COMPONENT, SUPP_ALI_COMPONENT, CHART_ALI_COMPONENT) |> 
+      rename(LLM_CONTEXT_ALI_COMPONENT = SUPP_ALI_COMPONENT)
+  ) |> 
+  pivot_longer(cols = c(ALI_COMPONENT:LLM_CONTEXT_ALI_COMPONENT), 
                names_to = "DATA", values_to = "COMPONENT")
 
 # Create new dataframe with number of missing values per ALI component
 num_miss = all_data |> 
   group_by(Variable_Name, DATA) |> 
-  summarize(Num_Missing = sum(is.na(COMPONENT)))
+  summarize(NUM_MISSING = sum(is.na(COMPONENT)))
 
 # Create bar plot of missing values per component (colored by wave)
 order_levels = num_miss |> 
   filter(DATA == "ALI_COMPONENT") |> 
-  arrange(desc(Num_Missing)) |> 
+  arrange(desc(NUM_MISSING)) |> 
   pull(Variable_Name)
-num_miss |> 
+bar_plot = num_miss |> 
   mutate(Variable_Name = factor(x = Variable_Name, 
                                 levels = order_levels, 
                                 labels = c("Creatinine Clearance", "Homo-\ncysteine",
@@ -36,24 +53,30 @@ num_miss |>
                                            "Systolic Blood Pressure", 
                                            "Diastolic Blood Pressure")), 
          DATA = factor(x = DATA, 
-                       levels = c("ALI_COMPONENT", "SUPP_ALI_COMPONENT", "CHART_ALI_COMPONENT"), 
+                       levels = c("ALI_COMPONENT", 
+                                  "CHART_ALI_COMPONENT", 
+                                  "ORIG_ALI_COMPONENT", 
+                                  "LLM_ALI_COMPONENT", 
+                                  "LLM_CONTEXT_ALI_COMPONENT"), 
                        labels = c("Unvalidated EHR Data", 
-                                  "EHR Data Supplemented with ICD Codes", 
-                                  "Chart Review Validation"))) |> 
+                                  "Chart Review Validation", 
+                                  "Augmented (Original Roadmap)", 
+                                  "Augmented (LLMs w/o Context Roadmap)",
+                                  "Augmented (LLMs w/ Context Roadmap)"))) |> 
   ggplot(aes(x = Variable_Name, 
-             y = Num_Missing, 
+             y = NUM_MISSING, 
              fill = DATA)) + 
   geom_bar(stat = "identity", 
            position = "dodge", 
            color = "black") + 
-  geom_text(aes(label=Num_Missing), 
+  geom_text(aes(label=NUM_MISSING), 
             vjust = -1, 
             size = 3, 
             position = position_dodge(width = 1)) + 
-  theme_minimal(base_size = 12) + 
+  theme_minimal(base_size = 14) + 
   labs(x = "Allostatic Load Index Component", 
        y = "Number of Patients", 
-       title = "A) Missing Values by Component") +
+       title = "Missing Values by Component") +
   theme(title = element_text(face = "bold"), 
         legend.position = "inside", 
         legend.position.inside = c(1, 0.9),
@@ -64,7 +87,105 @@ num_miss |>
   scale_fill_manual(values = cols, name = "Data:") + 
   scale_y_continuous(expand = expansion(mult = c(0, 0.1))) + 
   scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 8))
+bar_plot 
 
 ## Save it 
 ggsave(filename = "~/Documents/ehr-llm-validation/figures/missing_by_component.png", 
+       device = "png", width = 10, height = 6, units = "in")
+
+bar_plot + 
+  facet_wrap(~DATA) + 
+  theme(strip.background = element_rect(fill = "black"), 
+        strip.text = element_text(color = "white")) + 
+  scale_fill_manual(values = cols, guide = "none") 
+
+## Save it 
+ggsave(filename = "~/Documents/ehr-llm-validation/figures/missing_by_component_faceted.png", 
+       device = "png", width = 10, height = 6, units = "in")
+
+# Create bar plot of missing values per component (colored by wave)
+## But include chart review next to each one 
+num_miss |> 
+  filter(DATA != "CHART_ALI_COMPONENT") |> 
+  mutate(FAC_DATA = DATA) |> 
+  bind_rows(
+    num_miss |> 
+      filter(DATA == "CHART_ALI_COMPONENT") |> 
+      mutate(FAC_DATA = "ALI_COMPONENT")
+  ) |> 
+  bind_rows(
+    num_miss |> 
+      filter(DATA == "CHART_ALI_COMPONENT") |> 
+      mutate(FAC_DATA = "ORIG_ALI_COMPONENT")
+  ) |> 
+  bind_rows(
+    num_miss |> 
+      filter(DATA == "CHART_ALI_COMPONENT") |> 
+      mutate(FAC_DATA = "LLM_ALI_COMPONENT")
+  ) |> 
+  bind_rows(
+    num_miss |> 
+      filter(DATA == "CHART_ALI_COMPONENT") |> 
+      mutate(FAC_DATA = "LLM_CONTEXT_ALI_COMPONENT")
+  ) |> 
+  mutate(Variable_Name = factor(x = Variable_Name, 
+                                levels = order_levels, 
+                                labels = c("Creatinine Clearance", "Homo-\ncysteine",
+                                           "C-Reactive Protein", "Hemoglobin A1C", 
+                                           "Cholest-\nerol", "Trigly-\ncerides", 
+                                           "Serum Albumin", "Body Mass Index", 
+                                           "Systolic Blood Pressure", 
+                                           "Diastolic Blood Pressure")), 
+         DATA = factor(x = DATA, 
+                       levels = c("CHART_ALI_COMPONENT", 
+                                  "ALI_COMPONENT", 
+                                  "ORIG_ALI_COMPONENT", 
+                                  "LLM_ALI_COMPONENT", 
+                                  "LLM_CONTEXT_ALI_COMPONENT"), 
+                       labels = c("Chart Review Validation", 
+                                  "Unvalidated EHR Data", 
+                                  "Augmented (Original Roadmap)", 
+                                  "Augmented (LLMs w/o Context Roadmap)",
+                                  "Augmented (LLMs w/ Context Roadmap)")), 
+         FAC_DATA = factor(x = FAC_DATA, 
+                           levels = c("ALI_COMPONENT", 
+                                      "CHART_ALI_COMPONENT", 
+                                      "ORIG_ALI_COMPONENT", 
+                                      "LLM_ALI_COMPONENT", 
+                                      "LLM_CONTEXT_ALI_COMPONENT"), 
+                           labels = c("Unvalidated EHR Data", 
+                                      "Chart Review Validation", 
+                                      "Augmented (Original Roadmap)", 
+                                      "Augmented (LLMs w/o Context Roadmap)",
+                                      "Augmented (LLMs w/ Context Roadmap)"))) |> 
+  ggplot(aes(x = Variable_Name, 
+             y = NUM_MISSING, 
+             fill = DATA)) + 
+  geom_bar(stat = "identity", 
+           position = "dodge", 
+           color = "black") + 
+  geom_text(aes(label=NUM_MISSING), 
+            vjust = -1, 
+            size = 3, 
+            position = position_dodge(width = 1)) + 
+  facet_wrap(~FAC_DATA) + 
+  theme_minimal(base_size = 14) + 
+  labs(x = "Allostatic Load Index Component", 
+       y = "Number of Patients", 
+       title = "Missing Values by Component") +
+  theme(title = element_text(face = "bold"), 
+        legend.position = "inside", 
+        legend.position.inside = c(1, 0.9),
+        legend.text = element_text(size = 12), 
+        legend.title = element_text(size = 12, face = "bold"), 
+        legend.justification = "right", 
+        legend.background = element_rect(fill = "white"), 
+        strip.background = element_rect(fill = "black"), 
+        strip.text = element_text(color = "white")) + 
+  scale_fill_manual(values = cols, guide = "none") + 
+  scale_y_continuous(expand = expansion(mult = c(0, 0.1))) + 
+  scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 8)) 
+
+## Save it 
+ggsave(filename = "~/Documents/ehr-llm-validation/figures/missing_by_component_faceted_sidebyside_chartreview.png", 
        device = "png", width = 10, height = 6, units = "in")
