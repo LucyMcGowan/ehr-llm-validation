35969da578ad031cc2beac79dee2aca25ce1b889
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2025-08-18 18:24:47 -0400

    fix llm code to not make us hold onto 40 data frames

--- scripts/superset-llm-roadmap.R
+++ scripts/superset-llm-roadmap.R
@@ -1,58 +1,106 @@
-# Make one data frame of the superset of LLM roadmaps 
-## Load separate dataframes for each LLM roadmap 
-load("~/Documents/ehr-llm-validation/data-raw/no_context-llm.rda")
-dfs <- paste0("df_nocontext_", 1:20)
-df_list <- mget(dfs)
+library(dplyr)
+# Make one data frame of the superset of LLM roadmaps
+## Load separate dataframes for each LLM roadmap
+load(here::here("data-raw/no_context-llm.rda"))
 
 ## Create function to concatenate all the unique terms across all LLM roadmaps
-extract_terms <- function(df, i, variable_name) {
-  terms <- unlist(strsplit(paste(df |> 
-                                   filter(Variable_Name == variable_name) |> 
-                                   select(If_Missing_Search_For), collapse = ";"), ";"))
+extract_terms <- function(df, id_value, variable_name) {
+  terms <- unlist(strsplit(
+    paste(
+      df |>
+        filter(
+          id == glue::glue("df_nocontext_{id_value}"),
+          Variable_Name == variable_name
+        ) |>
+        select(If_Missing_Search_For),
+      collapse = ";"
+    ),
+    ";"
+  ))
   terms <- unique(stringr::str_trim(tolower(terms[terms != ""])))
-  data.frame(term = terms, df = paste0("df_", i))
+  data.frame(term = terms, df = paste0("df_", id_value))
 }
 
-## Extract terms for each of the 10 ALI components, and combine them into a 10-row dataframe 
-ali_components = c("ALB", "A1C", "BMI", "BP_SYSTOLIC", "BP_DIASTOLIC", "CHOL", "CREAT_C", "CRP", "HCST", "TRIG") ### List of components
+## Extract terms for each of the 10 ALI components, and combine them into a 10-row dataframe
+ali_components = c(
+  "ALB",
+  "A1C",
+  "BMI",
+  "BP_SYSTOLIC",
+  "BP_DIASTOLIC",
+  "CHOL",
+  "CREAT_C",
+  "CRP",
+  "HCST",
+  "TRIG"
+) ### List of components
 super_roadmap = data.frame() ### Initialize empty dataframe to hold superset roadmap
 
 for (comp in ali_components) {
-  all_terms_comp <- do.call(rbind, 
-                       Map(extract_terms, df_list, 1:20, comp)) |> 
-    pull(term) |> 
-    unique() |> 
+  all_terms_comp <- do.call(
+    rbind,
+    Map(extract_terms, list(df_nocontext), 1:20, comp)
+  ) |>
+    pull(term) |>
+    unique() |>
     paste(collapse = "; ")
-  super_roadmap = super_roadmap |> 
-    bind_rows(data.frame(Variable_Name = comp, 
-                         If_Missing_Search_For = all_terms_comp))
+
+  super_roadmap = super_roadmap |>
+    bind_rows(data.frame(
+      Variable_Name = comp,
+      If_Missing_Search_For = all_terms_comp
+    ))
 }
 
-## Save it 
-super_roadmap |> 
-  write.csv(here::here("data-raw/llm_nocontext_superset_roadmap.csv"), 
-            row.names = FALSE)
+## Save it
+super_roadmap |>
+  write.csv(
+    here::here("data-raw/llm_nocontext_superset_roadmap.csv"),
+    row.names = FALSE
+  )
 
-## Load separate dataframes for each LLM roadmap 
-load("~/Documents/ehr-llm-validation/data-raw/context-llm.rda")
-dfs <- paste0("df_context_", 1:20)
-df_list <- mget(dfs)
+## Load separate dataframes for each LLM roadmap
+load(here::here("data-raw/context-llm.rda"))
 
-## Extract terms for each of the 10 ALI components, and combine them into a 10-row dataframe 
+extract_terms <- function(df, id_value, variable_name) {
+  terms <- unlist(strsplit(
+    paste(
+      df |>
+        filter(
+          id == glue::glue("df_context_{id_value}"),
+          Variable_Name == variable_name
+        ) |>
+        select(If_Missing_Search_For),
+      collapse = ";"
+    ),
+    ";"
+  ))
+  terms <- unique(stringr::str_trim(tolower(terms[terms != ""])))
+  data.frame(term = terms, df = paste0("df_", id_value))
+}
+
+## Extract terms for each of the 10 ALI components, and combine them into a 10-row dataframe
 super_roadmap = data.frame() ### Initialize empty dataframe to hold superset roadmap
 
 for (comp in ali_components) {
-  all_terms_comp <- do.call(rbind, 
-                            Map(extract_terms, df_list, 1:20, comp)) |> 
-    pull(term) |> 
-    unique() |> 
+  all_terms_comp <- do.call(
+    rbind,
+    Map(extract_terms, list(df_context), 1:20, comp)
+  ) |>
+    pull(term) |>
+    unique() |>
     paste(collapse = "; ")
-  super_roadmap = super_roadmap |> 
-    bind_rows(data.frame(Variable_Name = comp, 
-                         If_Missing_Search_For = all_terms_comp))
+
+  super_roadmap = super_roadmap |>
+    bind_rows(data.frame(
+      Variable_Name = comp,
+      If_Missing_Search_For = all_terms_comp
+    ))
 }
 
-## Save it 
-super_roadmap |> 
-  write.csv(here::here("data-raw/llm_context_superset_roadmap.csv"), 
-            row.names = FALSE)
+## Save it
+super_roadmap |>
+  write.csv(
+    here::here("data-raw/llm_context_superset_roadmap.csv"),
+    row.names = FALSE
+  )
