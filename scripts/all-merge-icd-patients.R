e96631a8370767d034a6a1a7c9dd773a2e0e625a
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-08-19 11:57:03 -0400

    Add file paths to all-merge-icd-patients script

--- scripts/all-merge-icd-patients.R
+++ scripts/all-merge-icd-patients.R
@@ -1,8 +1,11 @@
 # Merge original roadmap into EHR + chart reviews 
 source("~/Documents/ehr-llm-validation/scripts/merge-icd-patients-orig-roadmap.R")
+## --> ali_dat_original_roadmap.csv
 
 # Merge LLMs (no context) roadmap into EHR + chart reviews 
 source("~/Documents/ehr-llm-validation/scripts/merge-icd-patients-llm-roadmap-no-context.R")
+## --> ali_dat_llm_nocontext_roadmap.csv
 
 # Merge LLMs (context) roadmap into EHR + chart reviews 
 source("~/Documents/ehr-llm-validation/scripts/merge-icd-patients-llm-roadmap-context.R")
+## --> ali_dat_llm_context_roadmap.csv
