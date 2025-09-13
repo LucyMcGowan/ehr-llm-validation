38b43128160ed71d086070d6babf388d281f519d
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-09-13 18:27:26 -0400

    Use EHR instead of chart review as first fork in flowcharts

--- figures/flowcharts.R
+++ figures/flowcharts.R
@@ -1,7 +1,7 @@
 # Load packages
 library(dplyr) ## To wrangle data
 library(tidyr) ## To transform data
-library(ggflowchart) ## To make a flowchart visualization
+library(flowchart) ## To make a flowchart visualization
 
 # Define colors
 paper_colors = c("#ff99ff", "#787ff6", "#8bdddb", "#7dd5f6", "#ffbd59")
@@ -13,7 +13,7 @@ val_pat_id = read.csv("~/Documents/Allostatic_load_audits/all_ali_dat.csv") |>
 
 # Write a function to load data and make flowchart comparing 
 ## Roadmap to chart review (in validated subset of 100 patients)
-make_flowchart = function(data_path, save_as, incl_ehr = FALSE) {
+make_flowchart = function(data_path, save_as, start_with_ehr = FALSE, then_chart_review = FALSE) {
   # Load data, contains: 
   ## Unvalidated ALI components (original extracted EHR data) for all 1000 patients -- "ALI_COMPONENT"
   ## Unvalidated ALI components augmented using audit roadmap for all 1000 patients -- SUPP_ALI_COMPONENT
@@ -60,26 +60,48 @@ make_flowchart = function(data_path, save_as, incl_ehr = FALSE) {
   #   rename(from = CHART_ALI_COMPONENT, 
   #          to = SUPP_ALI_COMPONENT)
   
-  if (incl_ehr) {
-    all_data |> 
-      as_fc(label = "Data points validated") |> 
-      fc_split(ALI_COMPONENT, show_zero = TRUE) |>
-      fc_split(CHART_ALI_COMPONENT, show_zero = TRUE) |>
-      fc_modify( # modifying only boxes 4 and 5
-        ~ . |>
-          mutate(
-            bg_fill = ifelse(id %in% c(1), paper_colors[5], bg_fill),
-            bg_fill = ifelse(id %in% c(2, 5, 9, 13, 15), paper_colors[1], bg_fill),
-            bg_fill = ifelse(id %in% c(3, 6, 10, 14, 16), paper_colors[2], bg_fill),
-            bg_fill = ifelse(id %in% c(4, 7, 11, 15, 17), paper_colors[3], bg_fill),
-            bg_fill = ifelse(id %in% c(8, 12, 16), paper_colors[4], bg_fill),
-            #text_color = ifelse(id == 4, "white", text_color),
-            #bg_fill = ifelse(id == 5, "violet", bg_fill)
-          )
-      ) |>
-      fc_draw() |> 
-      fc_export(here::here(save_as), 
-                width = 10000, height = 2500, res = 700) 
+  if (start_with_ehr) {
+    if (then_chart_review) {
+      all_data |> 
+        as_fc(label = "Data points validated") |> 
+        fc_split(ALI_COMPONENT, show_zero = TRUE) |>
+        fc_split(CHART_ALI_COMPONENT, show_zero = TRUE) |>
+        fc_modify( # modifying only boxes 4 and 5
+          ~ . |>
+            mutate(
+              bg_fill = ifelse(id %in% c(1), paper_colors[5], bg_fill),
+              bg_fill = ifelse(id %in% c(2, 5, 9, 13, 15), paper_colors[1], bg_fill),
+              bg_fill = ifelse(id %in% c(3, 6, 10, 14, 16), paper_colors[2], bg_fill),
+              bg_fill = ifelse(id %in% c(4, 7, 11, 15, 17), paper_colors[3], bg_fill),
+              bg_fill = ifelse(id %in% c(8, 12, 16), paper_colors[4], bg_fill),
+              #text_color = ifelse(id == 4, "white", text_color),
+              #bg_fill = ifelse(id == 5, "violet", bg_fill)
+            )
+        ) |>
+        fc_draw() |> 
+        fc_export(here::here(save_as), 
+                  width = 10000, height = 2500, res = 700) 
+    } else {
+      all_data |> 
+        as_fc(label = "Data points validated") |> 
+        fc_split(ALI_COMPONENT, show_zero = TRUE) |>
+        fc_split(SUPP_ALI_COMPONENT, show_zero = TRUE) |>
+        fc_modify( # modifying only boxes 4 and 5
+          ~ . |>
+            mutate(
+              bg_fill = ifelse(id %in% c(1), paper_colors[5], bg_fill),
+              bg_fill = ifelse(id %in% c(2, 5, 8, 11, 14), paper_colors[1], bg_fill),
+              bg_fill = ifelse(id %in% c(3, 6, 9, 12, 15), paper_colors[2], bg_fill),
+              bg_fill = ifelse(id %in% c(4, 7, 10, 13, 16), paper_colors[3], bg_fill),
+              #bg_fill = ifelse(id %in% c(8, 12, 16), paper_colors[4], bg_fill),
+              #text_color = ifelse(id == 4, "white", text_color),
+              #bg_fill = ifelse(id == 5, "violet", bg_fill)
+            )
+        ) |>
+        fc_draw() |> 
+        fc_export(here::here(save_as), 
+                  width = 10000, height = 2500, res = 700) 
+    }
   } else {
     all_data |> 
       as_fc(label = "Data points validated") |> 
@@ -106,20 +128,41 @@ make_flowchart = function(data_path, save_as, incl_ehr = FALSE) {
 # Make plots for chart review vs. EHR
 make_flowchart(data_path = "~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_original_roadmap.csv", 
                save_as = "~/Documents/ehr-llm-validation/figures/ehr_vs_chart_flowchart.png", 
-               incl_ehr = TRUE)
+               start_with_ehr = TRUE, 
+               then_chart_review = TRUE)
 
-# Make plots for original roadmap
+# Make plots for original roadmap vs. chart review
 make_flowchart(data_path = "~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_original_roadmap.csv", 
                save_as = "~/Documents/ehr-llm-validation/figures/chart_vs_original_flowchart.png")
 
-# Make plots for LLMs (without context) roadmap
+# Make plots for original roadmap vs. EHR
+make_flowchart(data_path = "~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_original_roadmap.csv", 
+               save_as = "~/Documents/ehr-llm-validation/figures/ehr_vs_original_flowchart.png", 
+               start_with_ehr = TRUE)
+
+# Make plots for LLMs (without context) roadmap vs. chart reviews
 make_flowchart(data_path = "~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_llm_nocontext_roadmap.csv", 
                save_as = "~/Documents/ehr-llm-validation/figures/chart_vs_llm_nocontext_flowchart.png")
 
-# Make plots for LLMs (with context) roadmap
+# Make plots for LLMs (without context) roadmap vs. EHR
+make_flowchart(data_path = "~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_llm_nocontext_roadmap.csv", 
+               save_as = "~/Documents/ehr-llm-validation/figures/ehr_vs_llm_nocontext_flowchart.png", 
+               start_with_ehr = TRUE)
+
+# Make plots for LLMs (with context) roadmap vs. chart review
 make_flowchart(data_path = "~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_llm_context_roadmap.csv", 
                save_as = "~/Documents/ehr-llm-validation/figures/chart_vs_llm_context_flowchart.png")
 
-# Make plots for LLMs (with context) + clinician roadmap
+# Make plots for LLMs (with context) roadmap vs. EHR
+make_flowchart(data_path = "~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_llm_context_roadmap.csv", 
+               save_as = "~/Documents/ehr-llm-validation/figures/ehr_vs_llm_context_flowchart.png", 
+               start_with_ehr = TRUE)
+
+# Make plots for LLMs (with context) + clinician roadmap vs. chart review
 make_flowchart(data_path = "~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_llm_context_clinician_roadmap.csv", 
                save_as = "~/Documents/ehr-llm-validation/figures/chart_vs_llm_context_clinician_flowchart.png")
+
+# Make plots for LLMs (with context) + clinician roadmap vs. EHR
+make_flowchart(data_path = "~/Documents/ehr-llm-validation/data-raw/patient_data/ali_dat_llm_context_clinician_roadmap.csv", 
+               save_as = "~/Documents/ehr-llm-validation/figures/ehr_vs_llm_context_clinician_flowchart.png", 
+               start_with_ehr = TRUE)
