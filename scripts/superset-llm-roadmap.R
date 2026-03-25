5c14b6021be4dc81a12f16c745697dd74d6c5888
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-03-25 14:15:16 -0400

    Fix new superset roadmaps

--- scripts/superset-llm-roadmap.R
+++ scripts/superset-llm-roadmap.R
@@ -6,7 +6,7 @@ library(dplyr)
 load(here::here("data-raw/no_context-llm-loop-icd10.rda"))
 
 ## Create function to concatenate all the unique terms across all LLM roadmaps
-extract_terms <- function(df, id_value, variable_name) {
+extract_terms_nocontext <- function(df, id_value, variable_name) {
   terms <- unlist(strsplit(
     paste(
       df |>
@@ -41,7 +41,7 @@ super_roadmap = data.frame() ### Initialize empty dataframe to hold superset roa
 for (comp in ali_components) {
   all_terms_comp <- do.call(
     rbind,
-    Map(extract_terms, list(df_nocontext), 1:20, comp)
+    Map(extract_terms_nocontext, list(df_nocontext), 1:20, comp)
   ) |>
     pull(term) |>
     unique() |>
@@ -67,10 +67,27 @@ load(here::here("data-raw/context-llm-loop-icd10.rda"))
 ## Extract terms for each of the 10 ALI components, and combine them into a 10-row dataframe
 super_roadmap = data.frame() ### Initialize empty dataframe to hold superset roadmap
 
+## Create function to concatenate all the unique terms across all LLM roadmaps
+extract_terms_context <- function(df, id_value, variable_name) {
+  terms <- unlist(strsplit(
+    paste(
+      df |>
+        filter(
+          id == glue::glue("df_context_{id_value}"),
+          Variable_Name == variable_name
+        ) |>
+        select(If_Missing_Search_For),
+      collapse = ";"
+    ),
+    ";"
+  ))
+  terms <- unique(stringr::str_trim(tolower(terms[terms != ""])))
+  data.frame(term = terms, df = paste0("df_", id_value))
+}
 for (comp in ali_components) {
   all_terms_comp <- do.call(
     rbind,
-    Map(extract_terms, list(df_context), 1:20, comp)
+    Map(extract_terms_context, list(df_context), 1:20, comp)
   ) |>
     pull(term) |>
     unique() |>
