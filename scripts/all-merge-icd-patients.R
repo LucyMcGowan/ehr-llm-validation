3c5afe8d7887182a3a9ac125f6dede3987569569
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-04-04 12:16:24 -0400

    Add new roadmaps to all merge ICD + patient data

--- scripts/all-merge-icd-patients.R
+++ scripts/all-merge-icd-patients.R
@@ -6,10 +6,22 @@ source("~/Documents/ehr-llm-validation/scripts/merge-icd-patients-orig-roadmap.R
 source("~/Documents/ehr-llm-validation/scripts/merge-icd-patients-llm-roadmap-no-context.R")
 ## --> ali_dat_llm_nocontext_roadmap.csv
 
+# Merge LLMs (no context, for loop + ICD-10) roadmap into EHR + chart reviews 
+source("~/Documents/ehr-llm-validation/scripts/merge-icd-patients-llm-roadmap-no-context-loop-icd10.R")
+## --> ali_dat_llm_nocontext_loop_icd10_roadmap.csv
+
 # Merge LLMs (context) roadmap into EHR + chart reviews 
 source("~/Documents/ehr-llm-validation/scripts/merge-icd-patients-llm-roadmap-context.R")
 ## --> ali_dat_llm_context_roadmap.csv
 
+# Merge LLMs (context, for loop + ICD-10) roadmap into EHR + chart reviews 
+source("~/Documents/ehr-llm-validation/scripts/merge-icd-patients-llm-roadmap-context-loop-icd10.R")
+## --> ali_dat_llm_context_loop_icd10_roadmap.csv
+
 # Merge LLMs (context) roadmap into EHR + chart reviews 
 source("~/Documents/ehr-llm-validation/scripts/merge-icd-patients-llm-roadmap-context-clinician.R")
 ## --> ali_dat_llm_context_clinician_roadmap.csv
+
+# Merge LLMs (context, for loop + ICD-10) roadmap into EHR + chart reviews 
+source("~/Documents/ehr-llm-validation/scripts/merge-icd-patients-llm-roadmap-context-loop-icd10-clinician.R")
+## --> ali_dat_llm_context_loop_icd10_clinician_roadmap.csv
